f42c2bf73fdb049f272e3bf278668c3f0d2d3a10
npricejones <price-jones@astro.utoronto.ca>
2017-11-23 13:50:22 -0500

    Added setup.py for installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import warnings
+
+setup(name='tagspace',
+      version='1.0.0',
+      description='Testing cluster finding on synthetic and actual chemical space',
+      author='Natalie Price-Jones',
+      author_email='price-jones@astro.utoronto.ca',
+      url='https://github.com/NatalieP-J/spectralspace',
+      package_dir = {'tagspace/': ''},
+      packages=['tagspace','tagspace/examples','tagspace/data','tagspace/clusters'],
+      package_data={'tagspace/data':['distributions/apogee/rc_photosphere_params.npy',
+                                     'distributions/apogee/rg_photosphere_params.npy']},
+      dependency_links = ['https://github.com/jobovy/apogee/tarball/master#egg=apogee',
+                          'https://github.com/jobovy/galpy/tarball/master#egg=galpy',
+                          'https://github.com/jobovy/isodist/tarball/master#egg=isodist'],
+      install_requires=['numpy','scipy','matplotlib','astropy',
+                        'apogee','galpy','isodist','scikit-learn']
+      )
+
+warnings.warn('''APOGEE installation requires environment variables to be set: SDSS_LOCAL_SAS_MIRROR, RESULTS_VERS, APOGEE_APOKASC_REDUX; see the package documentation for more details.''')
