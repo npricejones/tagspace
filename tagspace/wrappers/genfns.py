ebac19c0790fae245aa50fb606cdd51edac326ff
npricejones <price-jones@astro.utoronto.ca>
2017-12-04 14:15:30 -0500

    Added a uniform generation function

--- tagspace/wrappers/genfns.py
+++ tagspace/wrappers/genfns.py
@@ -1,15 +1,23 @@
-import numpy as np
+from __future__ import print_function
+import numpy as np		
 
 def normalgeneration(num=10,numelem=10,centers=np.zeros(10),
 					 stds=0.5*np.ones(10)):
 	"""
 	num: 			number of values to generate
 	numelem:		number of elements
-	means:			means around which to generate values 
-					if shape is numelem, then use same 
+	centers:		center (mean value) of the numelem-dimensional Gaussian
+	stds:			standard deviation of the numelem-dimensional Gaussian
 	"""
 	return np.random.randn(num,numelem)*stds + centers
 
 
-def uniformgeneration():
-	return None
+def uniformgeneration(num=10,numelem=10,minvals=-0.1*np.ones(10),
+					  maxvals=0.1*np.ones(10)):
+	"""
+	num:			number of values to generate
+	numelem:		number of elements
+	minvals:		min abundance value for each element
+	maxvals:		max abundance value for each element
+	"""
+	return np.random.random(size=(num,numelem))*(maxvals-minvals) + minvals
