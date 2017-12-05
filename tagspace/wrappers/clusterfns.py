55853fdab0f8242b870c048a7ff69f8b23fbbc9e
npricejones <price-jones@astro.utoronto.ca>
2017-12-04 19:10:50 -0500

    Fixed minor errors (missing self, kwarg references

--- tagspace/wrappers/clusterfns.py
+++ tagspace/wrappers/clusterfns.py
@@ -1,12 +1,12 @@
 import numpy as np
 # put user made clustering algorithms here to transform them into sklearn esque class objects
 
-class RandomAssign():
+class RandomAssign(object):
 	"""
 	Randomly assigns all stars to a cluster. If the number of clusters is not specified, 
 	the algorithm chooses a random number less than the total number of stars.
 	"""
-	def __init__(k = None):
+	def __init__(self,k = None):
 		"""
 		Assigns the number of clusters.
 
@@ -16,7 +16,7 @@ class RandomAssign():
 		self.k = k
 		return None
 
-	def fit_predict(clusterdata):
+	def fit_predict(self,clusterdata):
 		"""
 		Assigns each star in clusterdata to a random group. Will also update k
 		to be a random number less than the total number of samples if k is 
